7316e52aed3b2e0b8041a7a4524700448e400c84
Abhishikta <pradhanabhishikta41@gmail.com>
2021-12-08 02:07:38 +0530

    sdsd

--- weather.js
+++ weather.js
@@ -7,6 +7,63 @@ const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 
 
+searchButton.addEventListener('click',(e)=>
+{
+
+e.preventDefault();
+getweather(searchInput.value);
+searchInput.value='';
+
+
+
+});
+
+const getweather=async(city)=>
+{
+    try{
+        const response= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d24119cbfd4ced53a65d683bbb580182`,
+
+
+        {mode:'cors'}
+        );
+
+        const weatherData=await response.json();
+        console.log(weatherData);
+        const{name}=weatherData;
+        const{feels_like}=weatherData.main;
+        const{id,main}=weatherData.weather[0];
+        loc.textContent=name;
+        climate.textContent=main;
+        tempvalue.textContent=Math.round(feels_like-273);
+        if(id>200 && id<300){
+            tempicon.src="Thunderstorm.png"
+        }
+        else if(id>300 && id<500){
+            tempicon.src="Drizzle.png"
+        }
+        else if(id>500 && id<600){
+            tempicon.src="Rain.png"
+        }
+        else if(id>600 && id<700){
+            tempicon.src="Snow.png"
+        }
+        else if(id>700 && id<800){
+            tempicon.src="Atmosphere.png"
+        }
+        else if(id==800){
+            tempicon.src="Clear.png"
+        }
+        else {
+            tempicon.src="Clouds.png"
+        }
+    }
+
+catch(error){
+    alert('city not found');
+}
+};
+
+
 
 window.addEventListener("load", () => {
     let long;
@@ -30,7 +87,29 @@ window.addEventListener("load", () => {
                     loc.textContent = name;
                     climate.textContent = main;
                     tempvalue.textContent = Math.round(feels_like - 273)
-
+                    console.log(data); 
+                    if(id>200 && id<300){
+                        tempicon.src="Thunderstorm.png"
+                    }
+                    else if(id>300 && id<500){
+                        tempicon.src="Drizzle.png"
+                    }
+                    else if(id>500 && id<600){
+                        tempicon.src="Rain.png"
+                    }
+                    else if(id>600 && id<700){
+                        tempicon.src="Snow.png"
+                    }
+                    else if(id>700 && id<800){
+                        tempicon.src="Atmosphere.png"
+                    }
+                    else if(id==800){
+                        tempicon.src="Clear.png"
+                    }
+                    else {
+                        tempicon.src="Clouds.png"
+                    }
+                    console.log(data);
 
                 })
         }
